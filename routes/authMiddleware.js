e40a84af369c808d0702cfb1e9e9fcb5522a05be
jutnh <jutnhbr@gmail.com>
2022-11-17 16:11:13 +0100

    [+] Added Extra Check for req user

--- routes/authMiddleware.js
+++ routes/authMiddleware.js
@@ -5,7 +5,7 @@ module.exports.isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.status(401).json({ msg: 'You are not authorized to view this resource' });
+        res.status(401).json({msg: 'You are not authorized to view this resource'});
     }
 }
 
@@ -13,26 +13,30 @@ module.exports.isAdmin = (req, res, next) => {
     if (req.isAuthenticated() && req.user.admin) {
         next();
     } else {
-        res.status(401).json({ msg: 'You are not authorized to view this resource because you are not an admin.' });
+        res.status(401).json({msg: 'You are not authorized to view this resource because you are not an admin.'});
     }
 }
 
 module.exports.isAuthorized = async (req, res, next) => {
+
+    let user;
+
     let id = req.url.toString().replace("/patient/", "");
-   let patient = await Patient.findOne({ _id: id }, function (err, patient) {
+    let patient = await Patient.findOne({_id: id}, function (err, patient) {
         if (err) return (err);
         return (patient);
     });
-    let user = await req.user.toObject();
+    if (req.user) {
+        let user = await req.user.toObject();
+
+    }
     console.log(patient)
-    if (req.isAuthenticated() && (user.doctor) && user.name === patient.doctor ) {
+    if (req.isAuthenticated() && (user.doctor) && user.name === patient.doctor) {
 
         next();
-    }
-    else if (req.isAuthenticated() && (user.name === patient.name)) {
+    } else if (req.isAuthenticated() && (user.name === patient.name)) {
         next();
-    }
-    else {
+    } else {
         res.status(401).json({msg: 'You are not authorized to view this resource.'});
     }
 }
