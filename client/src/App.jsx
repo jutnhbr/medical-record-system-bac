af443d979e041ea0c1dab6bc102959e77a0536ab
jutnh <jutnhbr@gmail.com>
2022-11-17 16:09:50 +0100

    [+] Added Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import Home from "./pages/Home";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
+import Records from "./pages/Records";
+import MedicalBoard from "./pages/MedicalBoard";
+
 const App = () => {
 
     return (
-        <div> Hello Records :) </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/records" element={<Records/>}/>
+                <Route path="/medicalboard" element={<MedicalBoard/>}/>
+            </Routes>
+        </Router>
     );
 }
 
