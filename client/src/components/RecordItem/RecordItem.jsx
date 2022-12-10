31c61661a2cf16c51f2b82fa4854a975b772f2a3
jutnh <jutnhbr@gmail.com>
2022-12-10 22:19:09 +0100

    [+] Now Displays MarkDown Files instead of JSX

--- client/src/components/RecordItem/RecordItem.jsx
+++ client/src/components/RecordItem/RecordItem.jsx
@@ -1,44 +1,29 @@
 import "./RecordItem.css";
-import CustomDivider from "../CustomDivider/CustomDivider";
-import RecordFooter from "../RecordFooter/RecordFooter";
+import Markdown from 'markdown-to-jsx';
+import {useEffect, useState} from "react";
 
 const RecordItem = () => {
+
+    const [record, setRecord] = useState({md: ""});
+
+    // TODO: Fetch record path from API
+
+    useEffect(() => {
+        sessionStorage.setItem("id", "12345678")
+        // GET request using fetch inside useEffect React hook to get record
+        fetch("http://localhost:3000/data/records/record" + sessionStorage.getItem("id") + ".md")
+            .then(async (res) => {
+                const record = await res.json();
+                setRecord(record);
+            }
+        )
+    }, [])
+
 
     return (
         <>
             <div className="record-item-container">
-                <div className="record-item-title">
-                    <h2>Medical Record Title</h2>
-                </div>
-                <div className="record-item-header">
-                    <p>Dr. John Doe </p>
-                    <p>Kevin Example</p>
-                    <p>Status Active</p>
-                    <p>535467346</p>
-                </div>
-                <div className="record-item-body">
-                    <div className="record-item-body-category-header">
-                        <h3>Diagnosis</h3>
-                    </div>
-                    <div className="record-item-body-details">
-                        <p>Record Body Information about something very important stuff that is very important. Yes Very
-                            Impotent</p>
-                    </div>
-                    <div className="record-item-body-category-header">
-                        <h3>Prescription</h3>
-                    </div>
-                    <div className="record-item-body-details">
-                        <p>Record Body Information about something very important stuff that is very important. Yes Very
-                            Impotent</p>
-                    </div>
-                    <div className="record-item-body-category-header">
-                        <h3>Other Notes</h3>
-                    </div>
-                    <div className="record-item-body-details">
-                        <p>Record Body Information about something very important stuff that is very important. Yes Very
-                            Impotent</p>
-                    </div>
-                </div>
+                <Markdown>{record.md}</Markdown>
             </div>
         </>
     )
