8f4591b3d3cfe8741e627ac03608f3bc4a972e99
jutnh <jutnhbr@gmail.com>
2022-12-10 17:59:50 +0100

    [+] Added new Panel to container

--- client/src/components/DashboardContainer/DashboardContainer.jsx
+++ client/src/components/DashboardContainer/DashboardContainer.jsx
@@ -2,13 +2,19 @@ import UserList from "../UserList/UserList";
 import CreateUserPanel from "../CreateUserPanel/CreateUserPanel";
 import "./DashboardContainer.css";
 import MockPanel from "../MockPanel/MockPanel";
+import Box from "@mui/material/Box";
+import UpdateUserPanel from "../UpdateUserPanel/UpdateUserPanel";
 
 const DashboardContainer = ({users}) => {
     return (
         <div className={"container"}>
-            <UserList users={users} />
-            <CreateUserPanel />
-             <MockPanel />
+            <UserList users={users}/>
+            <Box
+                sx={{display: 'flex', alignItems: "center", flexDirection: "column"}}>
+                <CreateUserPanel/>
+                <UpdateUserPanel/>
+            </Box>
+            <MockPanel/>
         </div>
     )
 }
