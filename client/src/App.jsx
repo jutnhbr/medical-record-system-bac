9b36d9f6a541f3864839aabe0262cfcf1e8decb2
jutnh <jutnhbr@gmail.com>
2022-12-10 17:59:01 +0100

    [+] Added new Routes to Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Dashboard from "./pages/Dashboard";
 import Records from "./pages/Records";
 import MedicalBoard from "./pages/MedicalBoard";
+import UserAdministration from "./pages/UserAdministration";
+import Reauth from "./pages/Reauth";
 
 const App = () => {
 
@@ -13,6 +15,8 @@ const App = () => {
                 <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/records" element={<Records/>}/>
                 <Route path="/medicalboard" element={<MedicalBoard/>}/>
+                <Route path="/dashboard/reauth" element={<Reauth/>}/>
+                <Route path="/dashboard/usrmgmt" element={<UserAdministration/>}/>
             </Routes>
         </Router>
     );
